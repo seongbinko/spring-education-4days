4200a704e5736c033bf89e185944b6539f641993
koseongbin <wnrhd1082@gmail.com>
2020-05-28 14:51:53 +0900

    commit;

--- src/main/java/kr/re/kitri/hello/controller/UserController.java
+++ src/main/java/kr/re/kitri/hello/controller/UserController.java
@@ -3,9 +3,9 @@ package kr.re.kitri.hello.controller;
 import kr.re.kitri.hello.model.User;
 import kr.re.kitri.hello.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class UserController {
@@ -15,8 +15,19 @@ public class UserController {
 
     //post는 사용자 등록으로 간주한다.
     @PostMapping("/users")
-    public User registerUser(@RequestBody User user) {
+    public void registerUser(@RequestBody User user) {
         System.out.println(user);
-        return userService.registUser(user);
+        userService.registUser(user);
+    }
+
+    @GetMapping("/users")
+    public List<User> getAllUsers() {
+        return userService.getAllUsers();
+    }
+
+    @GetMapping("/users/{userId}")
+    public User viewUserDetails(@PathVariable String userId) {
+
+        return userService.getUserDetails(userId);
     }
 }
--- src/main/java/kr/re/kitri/hello/dao/UserDao.java
+++ src/main/java/kr/re/kitri/hello/dao/UserDao.java
@@ -1,13 +1,16 @@
 package kr.re.kitri.hello.dao;
 
 import kr.re.kitri.hello.model.User;
+import org.apache.ibatis.annotations.Mapper;
 import org.springframework.stereotype.Repository;
 
-@Repository
-public class UserDao {
+import java.util.List;
 
-    public User insertUser(User user) {
-        System.out.println("insert ok....");
-        return user;
-    }
+@Repository
+@Mapper
+public interface UserDao {
+    //return 타입에서 public은 의미없음 제거
+    void insertUser(User user);
+    List<User> selectAllUsers();
+    User selectUserByUserId(String userId);
 }
